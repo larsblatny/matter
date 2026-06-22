edb82a3f665d3533c14cbf8a726d651f23b5135d
Sophie Fontana <sophiefontana14@gmail.com>
2026-06-22 17:39:06 +0200

    added general sampling for multiple sampling domains

--- src/sampling/regular_sampling_particles_vdb.hpp
+++ src/sampling/regular_sampling_particles_vdb.hpp
@@ -94,4 +94,126 @@ void regularSampleParticlesFromVdb(S& sim, ObjectVdb& obj, T ppc = 4)
     sim.particles.x = samples;
 }
 
+template <typename S>
+#ifdef THREEDIM
+void regularSampleParticlesFromVdb(S& sim, std::vector<ObjectVdb> objects, T ppc = 8)
+#else
+void regularSampleParticlesFromVdb(S& sim, std::vector<ObjectVdb> objects, T ppc = 4)
+#endif
+{
+
+    debug("Regular sampling of particles from VDB...");
+
+    std::vector<TV> samples;
+
+    sim.sampling_start_idx.push_back(0);
+
+#ifdef THREEDIM
+    const T pSpacing = sim.dx / std::cbrt(ppc);
+#else
+    const T pSpacing = sim.dx / std::sqrt(ppc);
+#endif
+
+    for (int i = 0; i < objects.size(); ++i) {
+
+        ObjectVdb& obj = objects[i];
+
+        TV min_corner, max_corner;
+        obj.bounds(min_corner, max_corner);
+        TV L = max_corner - min_corner;
+
+        sim.sampling_start_idx.push_back(samples.size());
+
+        #ifdef THREEDIM
+        debug("    Min corner: ", min_corner(0), ", ", min_corner(1), ", ", min_corner(2));
+        debug("    Max corner: ", max_corner(0), ", ", max_corner(1), ", ", max_corner(2));
+
+        const int nx = int(L(0) / pSpacing);
+        const int ny = int(L(1) / pSpacing);
+        const int nz = int(L(2) / pSpacing);
+
+        for (int i = 0; i < nx; ++i)
+        for (int j = 0; j < ny; ++j)
+        for (int k = 0; k < nz; ++k)
+        {
+            TV point(
+                min_corner(0) + (i + T(0.5)) * pSpacing,
+                min_corner(1) + (j + T(0.5)) * pSpacing,
+                min_corner(2) + (k + T(0.5)) * pSpacing
+            );
+
+            if (obj.inside(point))
+                samples.push_back(point);
+        }
+
+        sim.particle_volume = pSpacing * pSpacing * pSpacing;
+        sim.particle_mass   = sim.rho * sim.particle_volume;
+
+        sim.Lx = L(0);
+        sim.Ly = L(1);
+        sim.Lz = L(2);
+
+        #else 
+
+        debug("    Min corner: ", min_corner(0), ", ", min_corner(1));
+        debug("    Max corner: ", max_corner(0), ", ", max_corner(1));
+
+        const int nx = int(L(0) / pSpacing);
+        const int ny = int(L(1) / pSpacing);
+
+        std::vector<TV> samples;
+
+        for (int i = 0; i < nx; ++i)
+        for (int j = 0; j < ny; ++j)
+        {
+            TV point(
+                min_corner(0) + (i + T(0.5)) * pSpacing,
+                min_corner(1) + (j + T(0.5)) * pSpacing
+            );
+
+            if (obj.inside(point))
+                samples.push_back(point);
+        }
+
+        sim.particle_volume = pSpacing * pSpacing;
+        sim.particle_mass   = sim.rho * sim.particle_volume;
+
+        sim.Lx = L(0);
+        sim.Ly = L(1);
+
+        #endif
+
+        sim.sampling_start_idx.push_back(samples.size());
+        debug("    object ", i, ": ", sim.sampling_start_idx[i + 1] - sim.sampling_start_idx[i], " particles");
+
+    }
+
+        TV union_min;
+        TV union_max;
+
+        for (int i = 0; i < objects.size(); ++i){
+            TV min;
+            TV max;
+            objects[i].bounds(min, max);
+            union_min = union_min.cwiseMin(min);
+            union_max = union_max.cwiseMax(max);
+        }
+
+        TV LU = union_max - union_min;
+        
+        sim.Lx = LU(0);
+        sim.Ly = LU(1);
+        #ifdef THREEDIM
+            sim.Lz = LU(2);
+        #endif
+
+        sim.Np = samples.size();
+        debug("    Number of particle samples: ", sim.Np);
+        debug("    Particle spacing: ", pSpacing);
+
+        sim.particles = Particles(sim.Np);
+        sim.particles.x = samples;
+
+}
+
 #endif // REGULAR_SAMPLING_VDB_HPP
--- src/sampling/sampling_particles.hpp
+++ src/sampling/sampling_particles.hpp
@@ -124,5 +124,144 @@
 
 #endif 
 
+#ifdef THREEDIM 
+
+    template <typename S>
+    void sampleParticles(S& sim, std::vector<TV> origins, std::vector<TV> sizes, T kRadius, T ppc = 8, std::uint32_t attempts = 30, std::uint32_t seed = 42) {
+        std::uint32_t kAttempts = attempts;
+        std::uint32_t kSeed     = seed;
+
+        debug("Sampling particles from ", origins.size(), " boxes...");
+
+        std::vector<std::array<T, 3>> samples;
+
+        sim.sampling_start_idx.push_back(0);
+
+        T total_volume = 0;   
+        T total_square_samples = 0;   
+
+        for (int i = 0; i < origins.size(); ++i) {
+
+            TV mn = origins[i];
+            TV mx = origins[i] + sizes[i];
+
+            std::array<T, 3> kXMin = std::array<T, 3>{{mn(0), mn(1), mn(2)}};
+            std::array<T, 3> kXMax = std::array<T, 3>{{mx(0), mx(1), mx(2)}};
+            std::vector<std::array<T, 3>> box_samples = thinks::PoissonDiskSampling(kRadius, kXMin, kXMax, kAttempts, kSeed);
+
+            total_volume += sizes[i](0) * sizes[i](1) * sizes[i](2);
+            total_square_samples += box_samples.size();
+
+            for (int p = 0; p < box_samples.size(); ++p)
+                samples.push_back(box_samples[p]);
+
+            sim.sampling_start_idx.push_back(samples.size());
+
+            debug("    box ", i, ": ", sim.sampling_start_idx[i + 1] - sim.sampling_start_idx[i], " particles");
+        }
+
+        sim.dx = std::cbrt(ppc / T(total_square_samples) * total_volume);
+        sim.particle_volume = sim.dx * sim.dx * sim.dx / ppc;
+        sim.particle_mass   = sim.rho * sim.particle_volume;
+
+        debug("    dx set to ", sim.dx);
+
+        TV dmin;
+        TV dmax;
+
+        for (int i = 1; i < origins.size(); ++i) {
+
+            dmin = dmin.cwiseMin(origins[i]);
+            dmax = dmax.cwiseMax(origins[i] + sizes[i]);
+
+        }
+
+        sim.Lx = dmax(0) - dmin(0);
+        sim.Ly = dmax(1) - dmin(1);
+        sim.Lz = dmax(2) - dmin(2);
+
+        debug("Lx Ly Lz = ", sim.Lx, " ", sim.Ly, " ", sim.Lz);
+
+        sim.Np = samples.size();
+        debug("    Number of particles samples: ", sim.Np);
+
+        sim.particles = Particles(sim.Np);
+        for (int p = 0; p < sim.Np; p++) {
+            for (int d = 0; d < 3; d++) {
+                sim.particles.x[p](d) = samples[p][d];
+            }
+        }
+
+    } // end sampleParticles (multiple objects)
+
+#else // TWODIM
+
+    template <typename S>
+    void sampleParticles(S& sim, std::vector<TV> origins, std::vector<TV> sizes,
+                         T kRadius, T ppc = 6, std::uint32_t attempts = 200, std::uint32_t seed = 42) {
+        std::uint32_t kAttempts = attempts;
+        std::uint32_t kSeed     = seed;
+
+        debug("Sampling particles from ", origins.size(), " boxes...");
+
+        std::vector<std::array<T, 2>> samples;
+
+        sim.sampling_start_idx.push_back(0);
+
+        T total_volume = 0;  
+        T total_square_samples = 0;   
+
+
+        for (int i = 0; i < origins.size(); ++i) {
+
+            TV mn = origins[i];
+            TV mx = origins[i] + sizes[i];
+
+            std::array<T, 2> kXMin = std::array<T, 2>{{mn(0), mn(1)}};
+            std::array<T, 2> kXMax = std::array<T, 2>{{mx(0), mx(1)}};
+            std::vector<std::array<T, 2>> box_samples = thinks::PoissonDiskSampling(kRadius, kXMin, kXMax, kAttempts, kSeed);
+
+            total_volume += sizes[i](0) * sizes[i](1);
+            total_square_samples += box_samples.size();
+
+            for (int p = 0; p < box_samples.size(); ++p)
+                samples.push_back(box_samples[p]);  
+
+            sim.sampling_start_idx.push_back(samples.size());
+
+            debug("    box ", i, ": ", sim.sampling_start_idx[i + 1] - sim.sampling_start_idx[i], " particles");
+        }
+
+        sim.dx = std::sqrt(ppc / T(total_square_samples) * total_volume);
+        sim.particle_volume = sim.dx * sim.dx / ppc;
+        sim.particle_mass   = sim.rho * sim.particle_volume;
+
+        debug("    dx set to ", sim.dx);
+
+        TV dmin;
+        TV dmax;
+
+        for (int i = 0; i < origins.size(); ++i) {
+            dmin = dmin.cwiseMin(origins[i]);
+            dmax = dmax.cwiseMax(origins[i] + sizes[i]);
+        }
+        
+        sim.Lx = dmax(0) - dmin(0);
+        sim.Ly = dmax(1) - dmin(1);
+
+        sim.Np = samples.size();
+        debug("    Number of particles samples: ", sim.Np);
+
+        sim.particles = Particles(sim.Np);
+
+        for (int p = 0; p < sim.Np; p++) {
+            for (int d = 0; d < 2; d++) {
+                sim.particles.x[p](d) = samples[p][d];
+            }
+        }
+
+    } // end sampleParticles (multiple objects)
+
+#endif
 
 #endif  // SAMPLING_PARTICLES_HPP
